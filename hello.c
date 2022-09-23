73075db31dd4c89f2e6ab230014908d7c889d5b4
bigperson <daein7076@naver.com>
2022-09-23 16:52:56 +0900

    change hello.c & hello.exe

--- hello.c
+++ hello.c
@@ -8,8 +8,9 @@ int main()
     int num3 = INT_MIN;
     long num4 = LONG_MIN;
     long long num5 = LLONG_MIN;
+    int num6 = 22;
 
-    printf("%d %d %d %ld %ld  %lld\n", num1, num2, num3, num4, LONG_MAX, num5);
+    printf("%d %d %d %ld %ld  %lld %d\n", num1, num2, num3, num4, LONG_MAX, num5, num6);
     
     return 0;
 }
